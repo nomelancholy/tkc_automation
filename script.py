8eb80e777c67c71ecaee51820c9919be50bc018e
TakeKnowledge <starmekey@naver.com>
2023-07-08 11:08:44 +0900

    Update script.py

--- script.py
+++ script.py
@@ -33,20 +33,20 @@ load_dotenv(find_dotenv())
 # good condition : 3~5 / bad condition : 7~10
 WAIT_TIME = 5
 
-ARTIST = "E-40"
-SONG_TITLE = "Whomp Whomp"
+ARTIST = "Erick Sermon"
+SONG_TITLE = "Don't Give Up"
 YEAR = '2002'
-COUNT = '2228'
+COUNT = '2252'
 # Take Knowledge's Choice #1832. J. Rawls - Blue #2 (2001) \
 FULL_TITLE = f"Take Knowledge's Choice #{COUNT}. {ARTIST} - {SONG_TITLE} ({YEAR})"
 
-FEATURING = "Keak Da Sneak"
+FEATURING = "Lyric"
 FEATURING_MESSAGE = '이 피쳐링한'
 
 # audio | video
 LINK_TYPE = 'audio'
 
-YOUTUBE_LINK = 'https://youtu.be/ksgm1RgvAms'
+YOUTUBE_LINK = 'https://youtu.be/RpBc1yQDmA0'
 IFRAME_LINK = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+YOUTUBE_LINK.split(
     '/')[3]+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
